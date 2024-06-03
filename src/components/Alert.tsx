e17f1d4e20bdfbdf95752c1d8f802bcaca9cbcab
Sam <samuelnganga.w@gmail.com>
2024-06-03 22:50:35 +0300

    Create custom Alert components

---
+++ src/components/Alert.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+
+interface AlertProps {
+    type: string;
+    message: string;
+}
+
+export default function CustomAlert({ type, message }: AlertProps) {
+    return (
+        <Box
+            sx={{
+                p: 1.5,
+                my: 1,
+                textAlign: "center",
+                border:
+                    type === "success"
+                        ? "1px solid #dff0d8"
+                        : "1px solid #f2dede",
+                borderRadius: "5px",
+                backgroundColor: type === "success" ? "#dff0d8" : "#f2dede",
+                color: type === "success" ? "darkgreen" : "darkred",
+            }}
+        >
+            {message}
+        </Box>
+    );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,6 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import BlurhashComponent from "./Blurhash";
+import CustomAlert from "./Alert";
 
-export { Footer, Header, Sidebar, BlurhashComponent };
+export { Footer, Header, Sidebar, BlurhashComponent, CustomAlert };
